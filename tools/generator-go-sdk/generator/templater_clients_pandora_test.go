23beb3832f89d0c0d7922aad8e5302bb4e4a19c3
Tom Bamford <tom@bamford.io>
2022-08-23 01:12:08 +0100

    Pandora client/methods tests

---
+++ tools/generator-go-sdk/generator/templater_clients_pandora_test.go
@@ -0,0 +1,40 @@
+package generator
+
+import (
+	"testing"
+)
+
+func TestTemplatePandoraClient(t *testing.T) {
+	input := ServiceGeneratorData{
+		packageName:       "somepackage",
+		serviceClientName: "ExampleClient",
+		source:            AccTestLicenceType,
+	}
+
+	actual, err := clientsPandoraTemplater{}.template(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	expected := `package somepackage
+
+import (
+	"github.com/hashicorp/go-azure-sdk/client/resourcemanager"
+	"github.com/hashicorp/go-azure-sdk/environments"
+)
+
+// acctests licence placeholder
+
+type ExampleClient struct {
+	Client  *resourcemanager.Client
+	baseUri string
+}
+
+func NewExampleClientWithBaseURI(endpoint string) ExampleClient {
+	return ExampleClient{
+		Client: resourcemanager.NewResourceManagerClient(environments.ApiEndpoint(endpoint)),
+		baseUri: endpoint,
+	}
+}`
+	assertTemplatedCodeMatches(t, expected, *actual)
+}
